3100fd79e490552988417c0ce9cd38286dd19c23
DavidDevGamer <noobgamery567@gmail.com>
2022-10-20 21:42:04 +0700

    Update > Something here

--- src/TungstenVn/SeasonPass/SeasonPass.php
+++ src/TungstenVn/SeasonPass/SeasonPass.php
@@ -16,23 +16,26 @@ use muqsit\invmenu\InvMenuHandler;
 use jojoe77777\FormAPI\SimpleForm;
 class SeasonPass extends PluginBase implements Listener {
 
-    public $levelApi;
+    	public $levelApi;
 	public function onEnable(): void{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-        if(!class_exists(InvMenu::class) || !class_exists(SimpleForm::class)){
-        	$this->getServer()->getLogger()->info("\n\n§cSeasonPass > Thiếu Lib InvMenu và FormAPI\n");
-            $this->getServer()->getPluginManager()->disablePlugin($this);
-            return;
-        }
-        $this->levelApi = $this->getServer()->getPluginManager()->getPlugin("MineLevel");
-        $this->saveDefaultConfig();
-        $cmds = new commands($this);
-        $this->getServer()->getCommandMap()->register("seasonpass", $cmds);
-        $this->getServer()->getPluginManager()->registerEvents($cmds,$this);
+        	if(!class_exists(InvMenu::class) || !class_exists(SimpleForm::class)){
+        		$this->getServer()->getLogger()->info("\n\n§cSeasonPass > Thiếu Lib InvMenu và FormAPI\n");
+            		$this->getServer()->getPluginManager()->disablePlugin($this);
+            		return;
+        	}
+        	$this->levelApi = $this->getServer()->getPluginManager()->getPlugin("MineLevel");
+		if(is_null($this->levelApi)){
+        		$this->getServer()->getLogger()->info("\n\n§cSeasonPass > Thiếu MineLevel\n");
+		}
+        	$this->saveDefaultConfig();
+        	$cmds = new commands($this);
+        	$this->getServer()->getCommandMap()->register("seasonpass", $cmds);
+        	$this->getServer()->getPluginManager()->registerEvents($cmds,$this);
         
-        if(!InvMenuHandler::isRegistered()){
-            InvMenuHandler::register($this);
-        }
+        	if(!InvMenuHandler::isRegistered()){
+            		InvMenuHandler::register($this);
+        	}
 	}
 
 }
